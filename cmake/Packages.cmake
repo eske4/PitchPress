7953bd801cb18fec77bd771f7781290466e74976
eske4 <eske.larsen@gmail.com>
2024-10-05 13:13:40 +0200

    Added custom cmake formatting

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -1,5 +1,5 @@
 message("Fetching packages")
 
-cpmaddpackage("gh:google/googletest@1.15.2")
-cpmaddpackage("gh:PortAudio/portaudio@19.7.0")
-cpmaddpackage("gh:celtera/libremidi@4.5.0")
+CPMAddPackage("gh:google/googletest@1.15.2")
+CPMAddPackage("gh:PortAudio/portaudio@19.7.0")
+CPMAddPackage("gh:celtera/libremidi@4.5.0")
